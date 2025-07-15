2cae705063357ce8b2d6baaf082722efbd48ba24
SoftosBr <henriquezuffo2@gmail.com>
2025-07-14 22:35:57 -0300

    feat: add command to make easier clean the studies folder

---
+++ scripts/clear_studies_folder.sh
@@ -0,0 +1,15 @@
+
+studies_folder="$HOME/studies"
+
+if [ ! -d "$studies_folder"  ]; then
+  echo "\n Studies folder doesn't exist"
+  exit 1
+fi	
+
+cd "$studies_folder"
+
+if cd "$studies_folder"; then 
+  rm -rf *
+fi
+
+

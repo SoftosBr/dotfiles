0e1cbbb342757ea37fb187fcb18bb96cf5bae83e
SoftosBr <henriquezuffo2@gmail.com>
2025-07-23 22:45:17 -0300

    feat: add ts LSP

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,6 +3,7 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
+      lspconfig.ts_ls.setup({})
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})

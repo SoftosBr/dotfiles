b219254aadb29d4e8f6e70bd564e1959698cb6d3
SoftosBr <henriquezuffo2@gmail.com>
2025-07-27 21:50:53 -0300

    fix: correcting package name and change to install each package separately

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ handle_cd_error() {
 packages_sound_system="alsa-lib alsa-utils pavucontrol mpv pulseaudio pipewire pipewire-pulse"
 packages_i3="dunst picom polybar rofi"
 packages_amd_driver="xf86-video-amdgpu mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon"
-packages_arch="fzf kitty neovim redshift stow htop btop dbus-python flameshot bat neofetch lxappearance npm python-pip python-wal python-pywalfox wal-telegram-git telegram-desktop unzip xdg-user-dirs nitrogen yazi dolphin flameshot feh vlc ttf-nerd-fonts-symbols noto-fonts-cjk noto-fonts-emoji noto-fonts ttf-hack-nerd ttf-firacode ttf-firacode-nerd lazygit obsidian discord clipcat zsh lightdm"
+packages_arch="fzf kitty neovim redshift stow htop btop dbus-python flameshot bat neofetch lxappearance npm python-pip python-pywal python-pywalfox wal-telegram-git telegram-desktop unzip xdg-user-dirs nitrogen yazi dolphin flameshot feh vlc ttf-nerd-fonts-symbols noto-fonts-cjk noto-fonts-emoji noto-fonts ttf-hack-nerd ttf-firacode ttf-firacode-nerd lazygit obsidian discord clipcat zsh lightdm"
 
 if grep -q "hypervisor" /proc/cpuinfo; then
   packages_i3=$(echo "$packages_i3" | sed's/picom//g')
@@ -49,15 +49,32 @@ if ! command -v yay &>/dev/null; then
   sleep 1 && clear
 fi
 
-echo "Installing Programs..."
-packages="${packages_i3} ${packages_amd_driver} ${packages_sound_system} ${packages_arch}"
-yay -Syu "${packages}" --needed --noconfirm
 
+echo "Installing i3 Programs..."
+yay -Syu "${packages_i3}" --needed --noconfirm
+sleep 1 && clear
+
+
+echo "Installing sound system Programs..."
+yay -Syu "${packages_sound_system}" --needed --noconfirm
+sleep 1 && clear
+
+
+echo "Installing amd_driver Programs..."
+yay -Syu "${packages_amd_driver}" --needed --noconfirm
+sleep 1 && clear
+
+
+echo "Installing arch Programs..."
+yay -Syu "${packages_arch}" --needed --noconfirm
 sleep 1 && clear
 
 echo "Organizing dotfiles and configurations..."
 xdg-user-dirs-update
-stow -d .. -t "$HOME" dotfiles --override='.*' --ignore='install.sh|root'
+stow -t "$HOME" scripts
+mv "$HOME/.config/i3/config" "$HOME/.config/i3/config.bak"
+stow -t "$HOME/.config" .config --override='.*'
+ln -s .zshrc "$HOME/.zshrc"
 sudo usermod -aG video,input "$USER"
 
 echo "Changing default shell to Zsh..."
